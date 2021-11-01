1c7f8ea5463179dd9c901b456a548e7efd13d851
Stephen Karpeles <stephen@stephenkarpeles.com>
2021-11-01 14:56:54 -0500

    Sign in / sign out

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import "firebase/compat/auth";
+var firebaseui = require('firebaseui');
+
+const uiConfig = {
+  apiKey: "AIzaSyBK7HENYQlL7VtmgelnD7irypxHSsR8Sw0",
+  authDomain: "interview-code-challenge.firebaseapp.com",
+  projectId: "interview-code-challenge",
+  callbacks: {
+    signInSuccessWithAuthResult: () => false,
+  },
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(uiConfig);
+} 
+
+var ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth())
+
+ui.start('#firebaseui-auth-container', uiConfig);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  function handleSignOut() {
+    firebase.auth().signOut();
+    setIsSignedIn(false);
+    window.location.reload(false);
+  }
+
+  useEffect(() => {
+    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
+      setIsSignedIn(!!user);
+    });
+    return () => unregisterAuthObserver();
+  }, []);
+
+  if (!isSignedIn) {
+    return (
+      <div>
+        <div id="firebaseui-auth-container"></div>
+      </div>
+    );
+  }
+  else if (isSignedIn) {
+    return (
+      <div className="signed-in-success">
+        <p>Welcome, you are now signed-in!</p>
+
+        <a className="btn" onClick={handleSignOut}>Sign Out</a>
+      </div>
+    );
+  }
 }
 
 export default App;
