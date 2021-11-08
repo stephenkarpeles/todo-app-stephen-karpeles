dcf2f86ac9e631f7c4ef543be6ea51a44e4a8ec3
Stephen Karpeles <stephen@stephenkarpeles.com>
2021-11-08 09:34:33 -0600

    Update styling and inputs

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TodoItem from './TodoItem'
 import firebase from "firebase/compat/app"
 import 'firebase/compat/firestore';
@@ -34,7 +34,6 @@ onAuthStateChanged(auth, (user) => {
     console.log(uid);
 
     var docRef = db.collection("users").doc(uid);
-    var data = {};
 
     docRef.get().then((doc) => {
         if (doc.exists) {
@@ -42,7 +41,11 @@ onAuthStateChanged(auth, (user) => {
         } else {
             // doc.data() will be undefined in this case
             console.log("No such document! Creating now.");
-            db.collection("users").doc(uid).set(data);
+
+            db.collection("users").doc("uid").set({
+              name: "userDoc"
+            })
+            console.log("Document written with ID: ", docRef.id);
         }
     }).catch((error) => {
         console.log("Error getting document:", error);
@@ -54,10 +57,81 @@ onAuthStateChanged(auth, (user) => {
 });
 
 const TodoList = () => {
+
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    db.collection('items').orderBy('datetime', 'desc').onSnapshot(snapshot => {
+      setTodos(snapshot.docs.map(doc => {
+        return {
+          id: doc.id,
+          name: doc.data().todo,
+          datatime: doc.data().datatime
+        }
+      }))
+    })
+
+  }, []);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    db.collection('items').add({
+      todo: input,
+      datetime: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    setInput('');
+  }
+
   return (
     <div>
       <h2>Your To-Do List</h2>
-      <TodoItem/>
+      
+      <form className="add-todo-form" noValidate>
+
+        <input
+          required
+          id="todo"
+          label="Enter ToDo"
+          name="todo"
+          type="text"
+          autoFocus
+          value={input}
+          onChange={event => setInput(event.target.value)}
+        />
+
+        <button
+          type="submit"
+          onClick={addTodo}
+          disabled={!input}
+        >
+          Add Todo
+      </button>
+
+      </form>
+
+       <ul className="todo-list">
+        {
+          todos.map(todo => (
+
+            <li key={todo.id} >
+
+              <div> 
+                {todo.name}
+                {todo.datetime}
+              </div>
+
+              <div>
+                <div aria-label="Edit">
+                  Edit
+                </div>
+              </div>
+
+            </li>
+          ))
+        }
+      </ul>
+
     </div>
   )
 }
